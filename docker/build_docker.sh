567a85749f80cada044c9f901cb41388ecbd826f
Dan Shiebler <danshiebler@gmail.com>
2023-12-16 09:17:25 -0500

    update

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -4,10 +4,11 @@ sudo ln -sf $HOME/.colima/default/docker.sock /var/run/docker.sock
 
 
 # cd docker
-# docker build --platform=linux/amd64 -t  danshiebler/private:zap_stable -f Dockerfile-local .;
+# docker build --platform=linux/amd64 -t  danshiebler/private:zap_local -f Dockerfile-local .;
 
 
 # Build and puah
 git add -A .; git commit -m "update"; git push; \
-  docker build --platform=linux/arm64 -t  danshiebler/private:zap_stable -f docker/Dockerfile-local .; \
-  docker image push  danshiebler/private:zap_stable
+  # docker build --platform=linux/arm64 -t  danshiebler/private:zap_local -f docker/Dockerfile-local .; \
+  docker build --platform=linux/arm64 -t  danshiebler/private:zap_local -f Dockerfile-local .; \
+  docker image push  danshiebler/private:zap_local
